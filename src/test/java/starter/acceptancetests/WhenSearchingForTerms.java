91b419356633374fa1b53414accacfd7861f39e0
Juan Ignacio Costas <juaniganciocostas@gmail.com>
2024-01-17 21:33:41 -0300

    Ahí va

--- src/test/java/starter/acceptancetests/WhenSearchingForTerms.java
+++ src/test/java/starter/acceptancetests/WhenSearchingForTerms.java
@@ -1,7 +1,9 @@
 package starter.acceptancetests;
 
+import net.serenitybdd.core.Serenity;
 import net.serenitybdd.junit5.SerenityJUnit5Extension;
 import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.abilities.BrowseTheWeb;
 import net.serenitybdd.screenplay.annotations.CastMember;
 import net.serenitybdd.screenplay.ensure.Ensure;
 import net.serenitybdd.screenplay.questions.page.TheWebPage;
@@ -10,6 +12,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import starter.actions.navigation.NavigateTo;
 import starter.actions.search.LookForInformation;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(SerenityJUnit5Extension.class)
 class WhenSearchingForTerms {
@@ -18,15 +21,23 @@ class WhenSearchingForTerms {
     Actor actor;
 
     @Test
-    @DisplayName("Should be able to search for red things")
+    @DisplayName("Should be able to search for Microsoft things")
     void searchForRedThings() {
+        boolean match;
+        Ensure ensure;
         actor.attemptsTo(
                 NavigateTo.theSearchHomePage(),
-                LookForInformation.about("red"),
-                Ensure.that(TheWebPage.title()).containsIgnoringCase("red")
+                LookForInformation.about("Microsoft"),
+                Ensure.that(TheWebPage.title()).containsIgnoringCase("Microsoft")
         );
+        System.out.println("------------------------------------------------------------------------------------------------------------------");
+        if (TheWebPage.title().answeredBy(actor).contains("Microsoft")) {
+            System.out.println("The title of the page matches with the search query");
+        } else {
+            System.out.println("The title of the page does not match with the search query");
+        }
     }
-
+/*
     @Test
     @DisplayName("Should be able to search for green things")
     void searchForGreenThings() {
@@ -35,5 +46,5 @@ class WhenSearchingForTerms {
                 LookForInformation.about("green"),
                 Ensure.that(TheWebPage.title()).containsIgnoringCase("green")
         );
-    }
+    }*/
 }
--- src/test/java/starter/actions/navigation/WikiPage.java
+++ src/test/java/starter/actions/navigation/WikiPage.java
@@ -3,5 +3,5 @@ package starter.actions.navigation;
 import net.serenitybdd.annotations.DefaultUrl;
 import net.serenitybdd.core.pages.PageObject;
 
-@DefaultUrl("https://duckduckgo.com/")
-public class DuckDuckGoHomePage extends PageObject {}
+@DefaultUrl("https://www.wikipedia.org//")
+public class WikiPage extends PageObject {}
